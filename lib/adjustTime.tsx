fc1e82bf4cb04ec8979a6895df0e7db42641311d
jroug <rougeris@gmail.com>
2024-11-11 17:31:29 +0200

    fixes

--- lib/adjustTime.tsx
+++ lib/adjustTime.tsx
@@ -5,14 +5,14 @@ export const adjustTime = (stTime: Date) => {
     const targetTime = new Date(stTime); // Create a new date based on stTime
     const hours = targetTime.getHours();
     const minutes = targetTime.getMinutes();
-
+    
     // Check the range and set the target time accordingly
     if ((hours >= 7 && hours < 11) || (hours === 11 && minutes < 30)) {
         // Between 7:00 AM and 11:30 AM
         targetTime.setHours(9, 0, 0, 0); // Set to 9:00 AM
     } else if ((hours === 11 && minutes >= 30) || (hours >= 12 && hours < 14)) {
         // Between 11:30 AM and 2:00 PM
-        targetTime.setHours(11, 31, 0, 0); // Set to 11:30 AM
+        targetTime.setHours(11, 30, 0, 0); // Set to 11:30 AM
     } else if ((hours >= 14 && hours < 16) || (hours === 16 && minutes < 30)) {
         // Between 2:00 PM and 4:30 PM
         targetTime.setHours(14, 0, 0, 0); // Set to 2:00 PM
