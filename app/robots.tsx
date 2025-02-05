fa310e1fd2e9c232a8e553365eba2b75b5541817
jroug <rougeris@gmail.com>
2025-02-05 15:08:32 +0200

    fix robots.tsx

--- app/robots.tsx
+++ app/robots.tsx
@@ -1,9 +1,10 @@
 import type { MetadataRoute } from 'next'
 
-const robots = process.env.NEXT_PUBLIC_ENV_NAME === 'live'
-  ? require('./robots.prod').default
-  : require('./robots.dev').default
+export default async function robots(): Promise<MetadataRoute.Robots> {
 
-export default function robotsConfig(): MetadataRoute.Robots {
-  return robots
+  const config = process.env.NEXT_PUBLIC_ENV_NAME === 'live'
+    ? await import('./robots.prod').then((module) => module.default)
+    : await import('./robots.dev').then((module) => module.default)
+
+  return config
 }
