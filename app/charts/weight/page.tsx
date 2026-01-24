275ba52b4516ef97a02d9900036a20fd669a9f20
Correct Name <correct@email.com>
2026-01-24 19:11:20 +0200

    fixes

---
+++ app/charts/weight/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import React from "react";
+import Header from "@/components/Header";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Weight chart",
+    },
+  },
+};
+
+const labels = [
+  "Week 1",
+  "Week 2",
+  "Week 3",
+  "Week 4",
+  "Week 5",
+  "Week 6",
+  "Week 7",
+  "Week 8",
+  "Week 9",
+  "Week 10",
+  "Week 11",
+  "Week 12",
+  "Week 13",
+  "Week 14",
+  "Week 15",
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Weekly average weight (kg)",
+      data: [
+        83.2,
+        82.9,
+        82.6,
+        82.3,
+        82.1,
+        81.9,
+        81.8,
+        71.6,
+        81.5,
+        81.4,
+        81.3,
+        81.2,
+        81.1,
+        81.0,
+        70.9,
+      ],
+      backgroundColor: "rgba(53, 162, 235, 0.6)",
+    },
+  ],
+};
+
+export default function ChartsWeightPage() {
+  const pageTitle = "Test title";
+  const pageContent = "<p>test content</p>";
+
+  return (
+    <main className="site-content full-width">
+      <Header backUrl="/homepage" title={pageTitle} />
+
+      <div className="verify-email pb-20" id="charts-weight">
+        <div className="container mx-auto">
+          <div className="about-us-section-wrap">
+            <div className="about-us-screen-full border-b-2 border-gray-200 mt-4">
+              <div className="mx-auto bg-white rounded-lg mt-4 mb-6">
+                <div className="setting-bottom-img p-0 mt-16">
+                  <div className="verify-email-img-sec">
+                    <div className="main-img-top">
+                      <Bar options={options} data={data} />  
+                    </div>
+                  </div>
+                </div>
+
+                <div
+                  className="page-content mt-4"
+                  dangerouslySetInnerHTML={{ __html: pageContent }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
